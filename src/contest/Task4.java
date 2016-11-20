151068d1ffdb05a1236b6debffe2f9b693f2ca22
gslav <gslav@users.noreply.github.com>
2016-11-20 18:44:55 +0300

    задания 4. выполнено

--- src/contest/Task4.java
+++ src/contest/Task4.java
@@ -1,13 +1,17 @@
 package contest;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by gSlav on 20.11.2016 13:26.
  */
 
+/*
+Input:
+9
+6 5 3 1 2 2 3 4 6
+*/
 public class Task4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -17,20 +21,64 @@ public class Task4 {
                 throw new NullPointerException("необходимо выполнение условия (1 >= N >= 100)");
 
             int[] a = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
             if (N != a.length)
-                throw new NullPointerException("Размер массива A должнеб быть равен N");
+                throw new NullPointerException("Размер массива A должна быть равен N");
 
             print(a);
 
             Arrays.sort(a);
 
-            print(a);
+            List<Integer> b = new ArrayList<>();
+            for (int i : a) {
+                b.add(0, i);
+                if (i > 6 || i < 1)
+                    throw new NullPointerException("Размер группы должно быть 1 <= A <= 6");
+            }
+
+            System.out.println(
+                    "Минимальное количество рядов = " +
+                    reOrganize(b));
+        }
+        catch (NullPointerException e){
+            System.err.println(e.getMessage());
         }
         catch (Exception e){
             System.err.println(e.getMessage());
         }
-
     }
+
+    private static int reOrganize(List<Integer> b) {
+        for (int i = 0; i < b.size(); i++) {
+            if (b.get(i) == 6)
+                continue;
+            else {
+                int j = 1;
+                do {
+                    if (b.get(i) + b.get(b.size() - j) == 6) {
+                        b.set(i, (b.get(i) + b.get(b.size() - j)));
+                        b.remove(b.size() - j);
+                    }
+                    else {
+                        j++;
+                        if (i == j)
+                            break;
+                    }
+                }
+                while (true) ;
+            }
+        }
+        print(b);
+        return b.size();
+    }
+
+    private static void print(List<Integer> a) {
+        for (int b:a) {
+            System.out.print(b + "\t");
+        }
+        System.out.println();
+    }
+
 
     private static void print(int[] a) {
         for (int b:a) {
